49417a5305b03eced673bc450a6d35fceefa1f43
kushal <chatterjeekushal89@gmail.com>
2024-11-18 11:51:20 +0530

    add file

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['images.unsplash.com','example.com','www.pexels.com','images.pexels.com',"twitter.com","via.placeholder.com","img.freepik.com","pagedone.io,assets.aceternity.com"]
+        domains: ['images.unsplash.com','example.com','www.pexels.com','images.pexels.com',"twitter.com","via.placeholder.com","img.freepik.com","pagedone.io","assets.aceternity.com",]
       },
 };
 
--- src/helpers/sendverifyemail.ts
+++ src/helpers/sendverifyemail.ts
@@ -1,31 +1,5 @@
 
-// import { resend } from '@/lib/resend';
-// import {ApiResponce} from '@/types/ApiResponce';
 
-// export async function sendVerifyEmail(email:string,username:string,otp:string):Promise<ApiResponce>{
-
-//   try {
-//       await resend.emails.send({
-//           from: 'Acme <onboarding@resend.dev>',
-//           to: email,
-//           subject: 'Verify your email address by otp ',
-//           html: `<strong>username ${username}, email : ${email}, yourotp: ${otp}</strong>`, 
-//       });
-
-//       return {
-//           success:true,
-//           message:"Verification email sent"
-//       }
-//   } catch (emailError) {
-
-//     console.log("error in sending email",emailError);
-
-//     return {
-//         success:false,
-//         message:"Failed to send verification email"
-//     }
-//   }
-// }
 
 import nodemailer from "nodemailer";
 
